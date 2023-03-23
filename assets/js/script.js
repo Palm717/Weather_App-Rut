0f7783528dedb40e631e5c53081a354f341a0681
Robert Palmer <61207668+Palm717@users.noreply.github.com>
2023-03-22 22:41:03 -0400

    change over

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// I AM FAR BEHIND
+
+// var baseURL = "https://api.openweathermap.org/data/2.5";
+// var apiKey = "3acc16ffae9e45df92a064e41646355f";
+// // var lat = '33.952602';
+// // var lon = '-84.549934';
+// var url = baseURL + "/weather?appid=" + apiKey;
+
+// function getLocation() {
+//   navigator.geolocation.getCurrentPosition(function (locationData) {
+//     // console.log(locationData.coords.latitude);
+//     // Make a request to the Open Weather Map API for the current local weather
+//     $.get(
+//       url +
+//         "&lat=" +
+//         locationData.coords.latitude +
+//         "&lon=" +
+//         locationData.coords.longitude
+//     ).then(function (data) {
+//       console.log(data);
+//     });
+//   });
+// }
